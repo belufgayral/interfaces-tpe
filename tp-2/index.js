51b7857600da737cb9d30f8b4048ae7a7e309c10
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2023-09-28 01:22:53 -0300

    making changes on the component structure, applying styles from inside and outside the component

--- tp-2/index.js
+++ tp-2/index.js
@@ -1,35 +1,49 @@
 class PrimaryActiveBtn extends HTMLElement {
     constructor() {
         super();
+
+        // Create a shadow DOM
         const shadow = this.attachShadow({ mode: 'open' });
-       
-        shadow.innerHTML = `
-            <style>
-            :host {
-                display: inline-flex;
-              }
-      
-              button {
-                padding: 10px 24px;
-                justify-content: center;
-                align-items: center;
-                gap: 10px;
-                border-radius: 13px;
-                background: var(--Secundario-L1, #478D76);
-                cursor: pointer;
-              }
-      
-              button:hover {
-                background-color: #2980b9;
-              }
-            </style>
-            <button><slot></slot></button>
-        `;
-        const button = shadow.querySelector('button');
+
+        // Include a button element as content
+        const button = document.createElement('button');
+        button.innerHTML = '<slot></slot>';
+        shadow.appendChild(button);
+
+        // Apply custom styles to the button element
+        button.style.backgroundColor = '#478D76';
+        button.style.color = 'white';
+        button.style.padding = '10px 20px';
+        button.style.border = 'none';
+        button.style.cursor = 'pointer';
+        // Add a click event listener
         button.addEventListener('click', () => {
             alert('Button clicked!');
         });
     }
+
+    // Define a getter and setter for the 'class' attribute
+    get class() {
+        return this.getAttribute('class');
+    }
+
+    set class(value) {
+        this.setAttribute('class', value);
+    }
+
+    // Listen for changes to the 'class' attribute and apply it to the button
+    attributeChangedCallback(name, newValue) {
+        if (name === 'class') {
+            const button = this.shadowRoot.querySelector('button');
+            button.className = newValue;
+        }
+    }
+
+    // Specify which attributes to observe for changes
+    static get observedAttributes() {
+        return ['class'];
+    }
 }
 
 customElements.define('primary-active-btn', PrimaryActiveBtn);
+
