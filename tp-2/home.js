b02a1ac410d50b9f3cbfaa0de9d268c8a37bffc2
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2023-10-05 17:27:46 -0300

    got to make the imgs to load

--- tp-2/home.js
+++ tp-2/home.js
@@ -1,4 +1,50 @@
-import gamesCardData from './mockedData'
+/* import { gamesCardData } from './mockedData' */
+
+const gamesCardData = [
+    {
+        id: 1,
+        title: 'Metal Gear Solid V',
+        genre: 'Aventura',
+        price: '300',
+        img: '',
+    },
+    {
+        id: 2,
+        title: 'Silent Hill 2',
+        genre: 'Terror',
+        price: '50',
+        img: '',
+    },
+    {
+        id: 3,
+        title: 'Death Stranding',
+        genre: 'Aventura',
+        price: '869',
+        img: '',
+    },
+    {
+        id: 4,
+        title: 'Rise of Tomb Raider',
+        genre: 'Aventura',
+        price: '320',
+        img: '',
+    },
+    {
+        id: 5,
+        title: 'Age of Empires II',
+        genre: 'Estrategia',
+        price: '79',
+        img: '',
+    },
+    {
+        id: 6,
+        title: 'Bioshock',
+        genre: 'Accion',
+        price: '119',
+        img: '',
+    },
+]
+
 
 function renderGameCards(genre) {
     const container = document.getElementById(genre.toLowerCase()); // Assuming genre names are in lowercase
@@ -21,7 +67,7 @@ function renderGameCards(genre) {
     // Loop through the filtered games and create card elements
     genreGames.forEach(game => {
         const card = document.createElement('div');
-        card.classList.add('card'); // You can define a "card" class in your CSS for styling
+        card.classList.add('cardGame'); // You can define a "cardGame" class in your CSS for styling
 
         // Create and populate the card content
         const title = document.createElement('h3');
@@ -30,19 +76,20 @@ function renderGameCards(genre) {
         const price = document.createElement('p');
         price.textContent = `Price: $${game.price}`;
 
-        // You can also add an image here if you have an image URL in your JSON data
-        // const img = document.createElement('img');
-        // img.src = game.img;
-        // img.alt = game.title;
+        // Create the image element
+        const img = document.createElement('img');
+        img.src = '../tp-2/css/images/cards/cat.png'; // Update the path to your image
+        img.alt = game.title;
 
         // Append content to the card
         card.appendChild(title);
         card.appendChild(price);
-        // card.appendChild(img);
+        card.appendChild(img);
 
         // Append the card to the container
         container.appendChild(card);
     });
+
 }
 
 // Call the function to render the adventure genre
