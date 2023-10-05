6554fd19b3b10938249019502ed7a57f5ded127c
Belen <belenfernandezgayral@gmail.com>
2023-10-05 01:30:29 -0300

    Implementado menú dinámico y algunas transiciones

---
+++ tp-2/share-options.js
@@ -0,0 +1,5 @@
+document.querySelector(".share-button").addEventListener("click", toggleOptions);
+
+function toggleOptions() {
+    document.querySelector(".share-options").classList.toggle("show");
+}
