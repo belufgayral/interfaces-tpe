7f7e58721482a1e7d70797b58a41d1569aad6f61
Belen <belenfernandezgayral@gmail.com>
2023-10-07 01:19:08 -0300

    animación de popup registro-exitoso

---
+++ tp-2/registro-exitoso.js
@@ -0,0 +1,31 @@
+const registrationPopup = document.querySelector(".registration-popup");
+const dot1 = document.querySelector("#dot-1");
+const dot2 = document.querySelector("#dot-2");
+const dot3 = document.querySelector("#dot-3");
+const popupBackground = document.querySelector(".popup-background");
+
+document.querySelector("#registration-btn").addEventListener("click", changeToHome);
+
+function changeToHome() {
+    registrationPopup.classList.remove("display-none");
+    popupBackground.classList.remove("display-none");
+
+    setTimeout(() => {
+        dot1.classList.remove("display-none");
+        dot1.classList.add("dot-animation");
+    }, 1300);
+
+    setTimeout(() => {
+        dot2.classList.remove("display-none");
+        dot2.classList.add("dot-animation");
+    }, 2300);
+
+    setTimeout(() => {
+        dot3.classList.remove("display-none");
+        dot3.classList.add("dot-animation");
+    }, 3300);
+
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 4500);
+}
