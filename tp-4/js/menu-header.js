761b9973559542f3187c2101b94414b38ffadf08
Belen <belenfernandezgayral@gmail.com>
2023-11-25 20:07:27 -0300

    agregado menú desplegable al header

--- tp-4/js/menu-header.js
+++ tp-4/js/menu-header.js
@@ -1,6 +1,8 @@
 const hr1 = document.querySelector("#menu-hr1");
 const hr2 = document.querySelector("#menu-hr2");
 const hr3 = document.querySelector("#menu-hr3");
+const menuDesplegable = document.querySelector("#menu-desplegable");
+const blackBg = document.querySelector("#black-bg");
 
 document.querySelector("#menu-hamburguesa").addEventListener("click", changeCross);
 
@@ -8,4 +10,6 @@ function changeCross() {
     hr1.classList.toggle('animation-rotate-positive');
     hr2.classList.toggle('animation-disappears');
     hr3.classList.toggle('animation-rotate-negative');
+    menuDesplegable.classList.toggle('no-visible');
+    blackBg.classList.toggle('no-visible');
 }
--- tp-4/js/parallax-header.js
+++ tp-4/js/parallax-header.js
@@ -1,6 +1,7 @@
 const logo = document.querySelector("#logo");
 const small_logo = document.querySelector("#small-logo");
 const header = document.querySelector("#header");
+const menuDesplegable = document.querySelector("#menu-desplegable");
 
 function stickyHeader(y) {
      //manipulamos el logo.
@@ -16,15 +17,19 @@ function stickyHeader(y) {
     }
 
     //manipulamos el z-index y el height del header.
-    if (y < 200) {
-        header.classList.add("z-index-2");
-        header.classList.remove("z-index-10");
+    if (y < 105) {
+        // header.classList.add("z-index-2");
+        // header.classList.remove("z-index-10");
         header.style.height = "118px";
+        menuDesplegable.style.top = "118px";
+        // menuDesplegable.style.background = "rgba(84, 152, 247, 0.8)";
     }
-    if (y > 200) {
-        header.classList.remove("z-index-2");
-        header.classList.add("z-index-10");
+    if (y > 105) {
+        // header.classList.remove("z-index-2");
+        // header.classList.add("z-index-10");
         header.style.height = "103px";
+        menuDesplegable.style.top = "103px";
+        // menuDesplegable.style.background = "linear-gradient(180deg, rgba(84, 153, 248, 0.0) 0%, rgba(84, 152, 247, 0.6) 5%, rgba(84, 152, 247, 1) 100%)";
     }
 }
 
